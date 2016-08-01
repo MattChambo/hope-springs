b04b6d897b2addb7918b1bcb859355dd04f9711c
Matthew Chamberlain <matthewwillliamchamberlain@gmail.com>
2016-08-02 11:46:33 +1200

    Work on login page

--- app/templates/login.php
+++ app/templates/login.php
@@ -10,22 +10,13 @@
 				<label for="loginUsername">User name:</label>
 				<input type="text" name="username" placeholder="Your user name" id="loginUsername" value="<?= isset($_POST['login']) ? $_POST['username'] : '' ?>">
 				<br>
-				<span id="userNameMessage">
-					<?php if( isset($usernameMessage) ): ?>
-          			<?= $emailMessage ?>
-          			<?php endif ?>
-          		</span><br>
+				<span id="userNameMessage"><?= isset($userNameMessage) ? $userNameMessage : '' ?></span><br>
 				<label for="loginpassword">Password:</label>
 				<input type="password" name="password" placeholder="Enter your password" id="loginPassword">
 				<br>
 				<span id="passwordMessage">
-					<?php if( isset($passwordMessage) ): ?>
-          			<?= $passwordMessage ?>
-          			<?php endif ?>
-
-          			<?php if( isset($loginMessage) ): ?>
-          			<?= $loginMessage ?>
-          			<?php endif ?>
+					<?= isset($passwordMessage) ? $passwordMessage : '' ?>
+          			<?= isset($loginMessage) ? $loginMessage : '' ?>
 				</span><br><br>
 				<input type="submit" name="login" value="Login now!" id="loginsubmit" class="btn btn-success">
 			</form>
